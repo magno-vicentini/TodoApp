df213ae9360a3ea1ae3ddc0f59552d5c2818cc57
Magno Vicentini <magno-vicentini@hotmail.com>
2021-03-18 22:38:05 -0300

    todoapp1

---
+++ src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+       <PageHeader name='Sobre' small='Nós'></PageHeader>
+           <h2>Nossa História</h2>
+           <p>É isso ai...</p>
+           <h2>Missão e Visão</h2>
+           <p>É isso ai...</p>
+           <h2>Imprensa </h2>
+           <p>É isso ai...</p>
+       
+    </div>
+)
