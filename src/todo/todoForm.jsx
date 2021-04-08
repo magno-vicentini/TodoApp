f324082975cd96cb2f817f3b81f43173ae861bf2
Magno Vicentini <magno-vicentini@hotmail.com>
2021-04-07 22:16:55 -0300

    TodoApp para Redux

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
+import { changeDescription} from './todoActions'
 
-export default props => {
+const TodoForm =  props => {
     const keyHandler = (e) => {
         if (e.key ==='Enter') {
             e.shiftKey ? props.handleSearch() : props.handleAdd()
@@ -14,7 +17,7 @@ export default props => {
         <div role='form' className='todoForm'>
             <Grid cols='12 9 10'>
                 <input id= 'description' className='form-control' placeholder= 'Adicione uma tarefa'
-                onChange={props.handleChange}
+                onChange={props.changeDescription}
                 onKeyUp={keyHandler}
                 value={props.description}/>  
             </Grid>
@@ -30,3 +33,6 @@ export default props => {
         </div>
     )
 }
+const mapStateToProps = state => ({description: state.todo.description})
+const mapDispatchToProps = dispatch => bindActionCreators({changeDescription}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
